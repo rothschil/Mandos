2681d62bbac27be6ff03fb8a78ccacd6dade3d4d
Sam <WCNGS@HOTMAIL.COM>
2021-04-23 16:28:25 +0800

    构造器绑定

---
+++ mandos-framework/framework-spring/dependency-injection/src/main/java/xyz/wongs/weathertop/dependency/injection/constructor/AutowireInjectioneDemo.java
@@ -0,0 +1,28 @@
+package xyz.wongs.weathertop.dependency.injection.constructor;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import xyz.wongs.weathertop.dependency.injection.BookHandler;
+
+/**
+ * Autowiring
+ * @author <a href="mailto:WCNGS@QQ.COM">Sam</a>
+ * @ClassName AutowireInjectioneDemo
+ * @Description
+ * @Github <a>https://github.com/rothschil</a>
+ * @date 2021/4/23 14:35
+ * @Version 1.0.0
+ */
+public class AutowireInjectioneDemo {
+
+    public static void main(String[] args) {
+
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        String path = "classpath:/META-INF/autowire-dependency-constructor-injection.xml";
+        reader.loadBeanDefinitions(path);
+
+        BookHandler bookHandler = beanFactory.getBean(BookHandler.class);
+        System.out.println(bookHandler);
+    }
+}
